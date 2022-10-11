10f4ac9ca55bda49d813f00979e5593785b4f4ac
cashmere53 <15913755+cashmere53@users.noreply.github.com>
2022-10-11 12:47:04 +0900

    update package and project file

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from re import Match, fullmatch
+from typing import Optional
+
 from template import __version__
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_version() -> None:
+    pyproject_file: Path = Path("./pyproject.toml")
+    with pyproject_file.open("r") as fp:
+        buffer: str = list(filter(lambda x: x.startswith("version"), fp))[0]
+    m_res: Optional[Match[str]] = fullmatch(r'version = "(.*)"\n', buffer)
+    if m_res is None:
+        raise ValueError("version string is not found in pyproject.toml")
+    version: str = m_res[1]
+
+    assert __version__ == version
