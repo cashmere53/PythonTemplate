b38585b9bfc65ef82b440b1b07cbfb0bb19f55e2
cashmere53 <15913755+cashmere53@users.noreply.github.com>
2023-10-21 23:46:20 +0900

    update

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
 
 from pathlib import Path
 from re import Match, fullmatch
-from typing import Optional
 
 from template import __version__
 
@@ -10,10 +9,11 @@ from template import __version__
 def test_version() -> None:
     pyproject_file: Path = Path("./pyproject.toml")
     with pyproject_file.open("r") as fp:
-        buffer: str = list(filter(lambda x: x.startswith("version"), fp))[0]
-    m_res: Optional[Match[str]] = fullmatch(r'version = "(.*)"\n', buffer)
+        buffer: str = next(filter(lambda x: x.startswith("version"), fp))
+    m_res: Match[str] | None = fullmatch(r'version = "(.*)"\n', buffer)
     if m_res is None:
-        raise ValueError("version string is not found in pyproject.toml")
+        msg = "version string is not found in pyproject.toml"
+        raise ValueError(msg)
     version: str = m_res[1]
 
     assert __version__ == version
