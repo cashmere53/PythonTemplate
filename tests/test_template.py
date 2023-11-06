7c7b0ff753a5e32bc7a8f59825310b52edd889df
cashmere53 <15913755+cashmere53@users.noreply.github.com>
2023-11-06 11:38:09 +0900

    update library dependencies

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-
 from pathlib import Path
-from re import Match, fullmatch
-from typing import Optional
 
-from template import __version__
+import tomllib as tl
 
 
 def test_version() -> None:
-    pyproject_file: Path = Path("./pyproject.toml")
-    with pyproject_file.open("r") as fp:
-        buffer: str = list(filter(lambda x: x.startswith("version"), fp))[0]
-    m_res: Optional[Match[str]] = fullmatch(r'version = "(.*)"\n', buffer)
-    if m_res is None:
-        raise ValueError("version string is not found in pyproject.toml")
-    version: str = m_res[1]
+    import template
 
-    assert __version__ == version
+    pyproject_file = Path("./pyproject.toml")
+    with pyproject_file.open("rb") as fp:
+        parsed_content = tl.load(fp)
+
+    correct_version = parsed_content["tool"]["poetry"]["version"]
+    assert template.__version__ == correct_version
